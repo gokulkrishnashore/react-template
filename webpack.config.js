c3a97ec85a2e144f7fd1df5c2e929fa092141521
gokulkrishnashore <gokul.krishna@shore.com>
2021-11-30 16:00:12 +0100

    Add: packtracker changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,37 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const PacktrackerPlugin = require("@packtracker/webpack-plugin");
 
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: './src/index.html',
-  filename: './index.html',
+  template: "./src/index.html",
+  filename: "./index.html",
 });
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
+  devtool: "cheap-module-eval-source-map",
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
     ],
   },
   optimization: {
     minimize: true,
-    minimizer: [
-      new UglifyJsPlugin(),
-    ],
+    minimizer: [new UglifyJsPlugin()],
     usedExports: true,
     sideEffects: true,
   },
-  plugins: [htmlPlugin],
+  plugins: [
+    htmlPlugin,
+    new PacktrackerPlugin({
+      project_token: "PT_PROJECT_TOKEN",
+      upload: true,
+      fail_build: true,
+    }),
+  ],
 };
